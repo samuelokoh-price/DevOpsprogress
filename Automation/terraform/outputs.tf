ba3422cd128e0a08ef986b06fcae20fffb309a8c
samuelokoh-price <paspagesamm@gmail.com>
2025-12-07 12:24:36 +0100

    edited some lines

--- Automation/terraform/outputs.tf
+++ Automation/terraform/outputs.tf
@@ -11,3 +11,9 @@ output "public_dns" {
   description = "The public DNS of the monitoring server"
   value       = aws_instance.monitoring.public_dns
 }
+
+# Show the public ID of the instance
+output "public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.my_ec2.public_ip
+}
